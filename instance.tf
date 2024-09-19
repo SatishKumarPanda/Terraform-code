3e6933d9c53ed1838d37a6fbd0e78254947f2c8c
Satish Kumar Panda <148872608+SatishKumarPanda@users.noreply.github.com>
2024-09-19 15:32:29 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -23,8 +23,7 @@ resource "aws_instance" "terraform" {
   key_name      = "All keypair"
   subnet_id     = "subnet-03a3f218c8dfa99fb"
 
-  vpc_security_group_ids = [aws_security_group.allow_rdp.id]  # Use vpc_security_group_ids instead of security_groups
-
+  
   tags = {
     Name = "TerraformInstance"
   }
